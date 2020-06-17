c36c1416da87ac1f817b6e2dc75e7e243cfa6608
younis-source <yalyahyai@gmail.com>
2020-06-17 19:38:52 +0400

    initial commit

---
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controllers
+{
+     [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+            
+        }
+         [HttpGet]
+         public async Task<IActionResult> GetValues() 
+         {
+             var values = await _context.Values.ToListAsync();
+             return Ok(values);
+         }
+
+        //  [HttpGet("{id}")]
+        //  public async Task< IActionResult> GetValue(int id)
+        //  {
+        //      var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+        //      return Ok(value);  
+        //  }
+        
+    }
+}
---
+++ ZwajApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Models;
+
+namespace ZwajApp.API.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+    
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
